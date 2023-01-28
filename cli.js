135d7f93e939a0bd66abf1c9fee1a26ec0691749
croberts808 <crobert7@email.unc.edu>
2023-01-28 14:02:51 -0500

    okay done trying for right now...

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import moment from "moment-timezone"
+import minimist from "minimist"
 
 const help_message = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE" + "\n" +
 "-h            Show this help message and exit." + "\n" + 
@@ -10,20 +11,11 @@ const help_message = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUD
 "-j            Echo pretty JSON from open-meteo API and exit." 
 
 //const [,, ...args] = process.argv
-//const args = process.argv.slice(2)
+const args = process.argv.slice(2)
 
-if (process.argv.slice(2).includes('-h')) {
-    console.log(help_message);
-    throw new Error('0')
-} else if (process.argv.slice(2).includes('-j')) {
-    console.log(`return JSON here`)
-    throw new Error('0')
-}
-const [options, north_south, latitude, east_west, longitude, timezone, timezone_value, days, days_value] = process.argv.slice(2)
-const args = [options, north_south, latitude, east_west, longitude, timezone, timezone_value]
-console.log(args)
-
-const timezone_values = timezone || moment.tz.guess()
+const days = args.d
+console.log(days)
+const timezone = args.z || moment.tz.guess()
 
 //const latitude = args.n //|| args.s
 //const longitude = args.w || args.e
@@ -35,22 +27,6 @@ const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52
 const data = await response.json();
 
     // -n
-if (north_south == '-n') {
-    const latitude_value = latitude
-} else if (north_south == '-s') {
-    const latitude_value = -1 * latitude
-}
-
-
-    // -h
-if (options == '-h') {
-    console.log(help_message)
-}
-    // -j
-if (options == '-j') {
-    console.log(data)
-}
-
 
 
     /* COMMAND LINE OPTIONS
@@ -73,4 +49,4 @@ if (days == 0) {
     console.log("tomorrow.")
   }
 
-console.log(`Latitude: ${latitude_value} \nLongitude: ${east_west} \nTimezone: ${timezone_values}`)
+//console.log(`Latitude: ${latitude_value} \nLongitude: ${east_west} \nTimezone: ${timezone_values}`)
