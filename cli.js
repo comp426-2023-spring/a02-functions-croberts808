ebb4aff491906ef5e12e51578784b4b3771a1b65
croberts808 <crobert7@email.unc.edu>
2023-03-02 15:08:56 -0500

    added if precipitation is null and brought back default values for latitude etc

--- cli.js
+++ cli.js
@@ -14,8 +14,8 @@ const help_message = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUD
 
 //const [,, ...args] = process.argv
 const args = minimist(process.argv.slice(2));
-var latitude;
-var longitude;
+var latitude = 99999;
+var longitude = 99999;
 
 if (args.n > 0) {
   latitude= args.n;
@@ -39,10 +39,12 @@ if (args.h) {
   process.exit(0);
 }
 
+
 // Make a request
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude+ '&daily=precipitation_hours&timezone=' + timezone);
 // Get the data from the request
 const data = await response.json();
+
 
 if (args.j) {
   console.log(data);
@@ -51,11 +53,14 @@ if (args.j) {
 
 let galosh = ""
 
-if (data.daily.precipitation_hours[days] > 0) {
-  galosh = "You might need your galoshes "
-} else {
-  galosh = "You will not need your galoshes "
+if (data.daily.precipitation_hours != null) {
+  if (data.daily.precipitation_hours[days] > 0) {
+    galosh = "You might need your galoshes "
+  } else {
+    galosh = "You will not need your galoshes "
+  }
 }
+
 
 if (days == 0) {
     console.log(galosh + "today.")
